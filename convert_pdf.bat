4c834040ce8975b919d5fcfa2b8514ba79f34d9f
TakehitoNishida <takehito.nishida@gmail.com>
2019-06-12 20:43:51 +0900

    bug fix

--- convert_pdf.bat
+++ convert_pdf.bat
@@ -1,6 +1,6 @@
 set fname_adc="Sample003.adoc"
 set fname_pdf="Sample003.pdf"
 
-if exist %fname_pdf% (rm %fname_pdf%)
+if exist %fname_pdf% (del %fname_pdf%)
 asciidoctor-pdf -r asciidoctor-pdf-cjk %fname_adc%
 pause
